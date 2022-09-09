1373f63c9cc733f09c0743d35536566d50ef8fa6
Daria <111886555+DariAleksandrovna@users.noreply.github.com>
2022-09-09 15:13:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 deposit = list(map(lambda x: int(x * money / 100), per_cent.values()))
 print(deposit)
 print("Максимальная сумма, которую вы можете заработать - ", deposit.max())
+
+# Или так
+# deposit.sort()
+# print("Максимальная сумма, которую вы можете заработать - ", deposit[-1])
