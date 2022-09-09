0b631e8dbebe8df6a62df7f7ce1015da98514a00
Daria <ms.darya2020@bk.ru>
2022-09-09 12:49:15 +0300

    add python practice

---
+++ main.py
@@ -0,0 +1,6 @@
+money = int(input("Введите сумму\n"))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = list(map(lambda x: int(x * money / 100), per_cent.values()))
+print(deposit)
+deposit.sort()
+print("Максимальная сумма, которую вы можете заработать - ", deposit[-1])
